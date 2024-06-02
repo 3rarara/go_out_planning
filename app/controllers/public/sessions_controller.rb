6373f10c6b58f37a4dcb19a5396adf01d9e37cdb
3rarara <161809435+3rarara@users.noreply.github.com>
2024-06-02 03:07:32 +0000

    user_edit

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :user_status, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -18,7 +19,29 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
+  # ログイン後トップページへ
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  # ログアウト後ログインページへ
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
   # protected
+  private
+  # アクティブ判断メソッド
+  def user_status
+    #emailからアカウントを１件取得
+    user = User.find_by(email: params[:user][:email])
+    return if user.nil?
+    return unless user.valid_password?(params[:user][:password])
+    return if user.is_active?
+    # flash[:alert] = "退会済みです。別のメールアドレスをお使いください。"
+    redirect_to new_user_registration_path
+  end
+
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,4 +1,6 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_current_user
 
   def mypage
   end
@@ -6,13 +8,32 @@ class Public::UsersController < ApplicationController
   def edit
   end
 
+  def update
+    @user.update(user_params)
+    redirect_to mypage_path
+  end
+
   def confirm
   end
 
-  def close_acount
+  def close_account
+    @user.update(is_active: false)
+    reset_session
+    # flash[:notice] = "退会処理を実行しました"
+    redirect_to new_user_registration_path
   end
 
   def show
+  end
+
+  private
+
+  def set_current_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 
 end
