b4a6b07488223fb3a92c60c90f85607b4257fb13
3rarara <161809435+3rarara@users.noreply.github.com>
2024-06-02 13:34:35 +0000

    fix/no_image

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,8 @@ class User < ApplicationRecord
 
   def get_profile_image(width, height)
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.png')
-      profile_image.attach(io: File.open(file_path), filename: 'no_image.png', content_type: 'image/png')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpg')
     end
     profile_image.variant(resize_to_fill: [width, height]).processed
   end
