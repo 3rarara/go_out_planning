d01709f1a99f32b9afca4abccf37990b8fad8ade
3rarara <161809435+3rarara@users.noreply.github.com>
2024-06-06 15:28:49 +0000

    fix/routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  def route_not_found
+    raise ActionController::RoutingError.new('Not Found')
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -29,4 +29,6 @@ Rails.application.routes.draw do
     end
   end
 
+  match '*path', to: 'application#route_not_found', via: :all
+
 end
