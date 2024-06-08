70a06152534b60d1395abb37af658735c7da1cf9
3rarara <161809435+3rarara@users.noreply.github.com>
2024-06-08 15:17:32 +0000

    feature/admin

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -18,6 +18,16 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
 
+  # ログイン後トップページへ
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  # ログアウト後ログインページへ
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
+
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
