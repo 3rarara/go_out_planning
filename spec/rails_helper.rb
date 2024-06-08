a793c60e097b6f22937843c742a3e6afa52f94d9
3rarara <161809435+3rarara@users.noreply.github.com>
2024-06-08 07:53:19 +0000

    add/rspec_test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,5 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+  config.include Devise::Test::IntegrationHelpers, type: :system
 end
