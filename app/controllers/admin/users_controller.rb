eafb0c785c927fb7b9ac0ef785bf26b9ea6b819a
3rarara <161809435+3rarara@users.noreply.github.com>
2024-06-10 01:14:10 +0000

    add/admin_close_account

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,32 +1,43 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :set_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.all
   end
 
-  def show
-    @user = User.find(params[:id])
-    @plans = @user.plans.all
-  end
-
   def edit
-    @user = User.find(params[:id])
     @plans = @user.plans.all
   end
 
   def update
-    user = User.find(params[:id])
-    if user.update(user_params)
+    if @user.update(user_params)
       flash[:notice] = "ユーザー情報を変更しました。"
-      redirect_to admin_user_path(user)
+      redirect_to edit_admin_user_path
     else
-      @user = User.find(params[:id])
+      @plans = @user.plans.all
       flash.now[:alert] = "ユーザー情報を変更できませんでした。"
       render 'edit'
     end
   end
 
+  def destroy
+    if @user.is_active == false
+      @user.destroy
+      flash[:notice] = "ユーザーを削除しました。"
+      redirect_to root_path
+    else
+      @plans = @user.plans.all
+      flash.now[:alert] = "ユーザーを削除できませんでした。"
+      render 'edit'
+    end
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
    params.require(:user).permit(:is_active)
--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,4 +1,5 @@
 class Public::LikesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_plan
 
   def create
