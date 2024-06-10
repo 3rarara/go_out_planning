26ebb94a2a1134c6dbc14eb64143cdda1d3c8632
3rarara <161809435+3rarara@users.noreply.github.com>
2024-06-10 04:43:38 +0000

    fix/move_index

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -3,9 +3,9 @@ class Admin::SearchesController < ApplicationController
   def search
     @range = params[:range]
 
-    if @range == "ユーザー"
+    if @range == "User"
       @users = User.looks(params[:search], params[:word])
-    elsif @range == "プラン"
+    elsif @range == "Plan"
       @plans = Plan.looks(params[:search], params[:word])
     end
   end
--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -4,9 +4,9 @@ class Public::SearchesController < ApplicationController
   def search
     @range = params[:range]
 
-    if @range == "ユーザー"
+    if @range == "User"
       @users = User.looks(params[:search], params[:word])
-    elsif @range == "プラン"
+    elsif @range == "Plan"
       @plans = Plan.looks(params[:search], params[:word])
     end
   end
