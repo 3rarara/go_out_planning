3bb0cc9a210123bad61aee03ecd2c1dda30e9336
3rarara <161809435+3rarara@users.noreply.github.com>
2024-06-11 05:02:49 +0000

    feature/async:いいねボタンの非同期化

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -3,15 +3,15 @@ class Public::LikesController < ApplicationController
   before_action :set_plan
 
   def create
-    like = current_user.likes.new(plan_id: @plan.id)
-    like.save
-    redirect_to request.referer
+    @like = current_user.likes.new(plan_id: @plan.id)
+    @like.save
+    # redirect_to request.referer
   end
 
   def destroy
-    like = current_user.likes.find_by(plan_id: @plan.id)
-    like.destroy
-    redirect_to request.referer
+    @like = current_user.likes.find_by(plan_id: @plan.id)
+    @like.destroy
+    # redirect_to request.referer
   end
 
   private
