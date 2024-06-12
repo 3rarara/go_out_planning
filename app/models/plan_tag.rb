6eeb266bfd247110eeddf53db80dcf752dbcbb61
3rarara <161809435+3rarara@users.noreply.github.com>
2024-06-12 06:24:07 +0000

    feature/tags:マイグレーションファイル作成

---
+++ app/models/plan_tag.rb
@@ -0,0 +1,2 @@
+class PlanTag < ApplicationRecord
+end
---
+++ test/models/plan_tag_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PlanTagTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
