d63d976267e80ab6b322079eadbcfb1647e962e1
3rarara <161809435+3rarara@users.noreply.github.com>
2024-06-13 05:29:24 +0000

    feature/chat:コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'chats/show'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # ユーザー用　URL/users/sign_in...
