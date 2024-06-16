357b51196f54bc7f6056d9ab3ccab4fa7338c1a9
3rarara <161809435+3rarara@users.noreply.github.com>
2024-06-16 14:21:28 +0000

    feature/draft:カラムの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,49 +27,49 @@ lucas = User.find_or_create_by!(email: "lucas@example.com") do |user|
   user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")
 end
 
-Plan.find_or_create_by!(title: "大阪旅行") do |plan|
-  plan.body = "大阪旅行で御朱印巡りをしました！おすすめのお寺や神社を紹介してます！移動手段は電車です！。"
-  plan.user = olivia
+# Plan.find_or_create_by!(title: "大阪旅行") do |plan|
+#   plan.body = "大阪旅行で御朱印巡りをしました！おすすめのお寺や神社を紹介してます！移動手段は電車です！。"
+#   plan.user = olivia
 
-  plan.plan_details.build(
-    title: "大阪駅 到着",
-    body: "10:00集合 徒歩10分移動"
-  )
+#   plan.plan_details.build(
+#     title: "大阪駅 到着",
+#     body: "10:00集合 徒歩10分移動"
+#   )
 
-  plan.plan_details.build(
-    title: "＊＊神社 到着",
-    body: "10:10早速御朱印を１つゲット"
-  )
-end
+#   plan.plan_details.build(
+#     title: "＊＊神社 到着",
+#     body: "10:10早速御朱印を１つゲット"
+#   )
+# end
 
-Plan.find_or_create_by!(title: "東京デートおすすめスポット️") do |plan|
-  plan.body = "東京でのカフェ巡り！いろんなカフェを紹介してます！デートにおすすめです。"
-  plan.user = james
+# Plan.find_or_create_by!(title: "東京デートおすすめスポット️") do |plan|
+#   plan.body = "東京でのカフェ巡り！いろんなカフェを紹介してます！デートにおすすめです。"
+#   plan.user = james
 
-  plan.plan_details.build(
-    title: "東京駅 到着",
-    body: "11:00集合 徒歩5分移動"
-  )
+#   plan.plan_details.build(
+#     title: "東京駅 到着",
+#     body: "11:00集合 徒歩5分移動"
+#   )
 
-  plan.plan_details.build(
-    title: "＊＊カフェ 到着",
-    body: "11:05 早めのランチ"
-  )
-end
+#   plan.plan_details.build(
+#     title: "＊＊カフェ 到着",
+#     body: "11:05 早めのランチ"
+#   )
+# end
 
-Plan.find_or_create_by!(title: "鎌倉のお寺へ行くぞ️") do |plan|
-  plan.body = "鎌倉でまったりデートしました。"
-  plan.user = lucas
+# Plan.find_or_create_by!(title: "鎌倉のお寺へ行くぞ️") do |plan|
+#   plan.body = "鎌倉でまったりデートしました。"
+#   plan.user = lucas
 
-  plan.plan_details.build(
-    title: "鎌倉駅 到着",
-    body: "11:00集合 徒歩20分移動"
-  )
+#   plan.plan_details.build(
+#     title: "鎌倉駅 到着",
+#     body: "11:00集合 徒歩20分移動"
+#   )
 
-  plan.plan_details.build(
-    title: "＊＊寺 到着",
-    body: "11:05 11:00御朱印ゲット！"
-  )
-end
+#   plan.plan_details.build(
+#     title: "＊＊寺 到着",
+#     body: "11:05 11:00御朱印ゲット！"
+#   )
+# end
 
-puts 'データを追加しました'
+puts '初期データを追加しました'
