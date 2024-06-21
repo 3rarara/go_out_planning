cc8fb1ef78a998e7680e4c6aa71d5953e5057081
3rarara <161809435+3rarara@users.noreply.github.com>
2024-06-21 06:23:07 +0000

    フェーズ３中間デプロイマイグレーションファイル修正

--- db/migrate/20240618131526_add_indexes_to_plans_and_plan_details.rb
+++ db/migrate/20240618131526_add_indexes_to_plans_and_plan_details.rb
@@ -1,8 +1,8 @@
 class AddIndexesToPlansAndPlanDetails < ActiveRecord::Migration[6.1]
   def change
     add_index :plans, :title
-    add_index :plans, :body
+    add_index :plans, :body, length: 255
     add_index :plan_details, :title
-    add_index :plan_details, :body
+    add_index :plan_details, :body, length: 255
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_20_034113) do
+ActiveRecord::Schema.define(version: 2024_06_18_131732) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -73,14 +73,6 @@ ActiveRecord::Schema.define(version: 2024_06_20_034113) do
   create_table "likes", force: :cascade do |t|
     t.integer "plan_id", null: false
     t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "maps", force: :cascade do |t|
-    t.string "address"
-    t.float "latitude"
-    t.float "longitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
