bce817385c694c33f32b191ab0939880544d5ffc
3rarara <161809435+3rarara@users.noreply.github.com>
2024-06-21 01:54:37 +0000

    fix/data_acquisition:モデル・コントローラー close #84

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -2,8 +2,9 @@ class Public::NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    # ユーザーのすべての通知を取得し、作成日時の降順に並べる
     @notifications = current_user.notifications.order(created_at: :desc)
-    @notifications_read = current_user.notifications.where(read: false).order(created_at: :desc)
+    @notifications_read = @notifications.where(read: false)
   end
 
   def mark_all_as_read
