c6d362dd3951c706b9f06bfa6c6ec29c080be036
3rarara <161809435+3rarara@users.noreply.github.com>
2024-06-23 13:14:07 +0000

    fix/search_plan

--- db/migrate/20240623124004_add_not_null_to_plan_search_in_plans.rb
+++
@@ -1,5 +0,0 @@
-class AddNotNullToPlanSearchInPlans < ActiveRecord::Migration[6.1]
-  def change
-    change_column :plans, :plan_search, :string, null: false, default: ''
-  end
-end
--- db/migrate/20240623124033_add_index_to_plan_search_in_plans.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToPlanSearchInPlans < ActiveRecord::Migration[6.1]
-  def change
-    add_index :plans, :plan_search
-  end
-end
