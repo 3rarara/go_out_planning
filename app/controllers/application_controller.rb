32bfe063e10a7c109138ce0ae2bb71c8696399aa
3rarara <161809435+3rarara@users.noreply.github.com>
2024-06-26 16:33:28 +0000

    fix/notification_count:アイコンバッチ追加 close #108

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_unread_notifications_count
+
+  private
+
+  def set_unread_notifications_count
+    @unread_notifications_count = user_signed_in? ? current_user.notifications.where(read: false).count : 0
+  end
 end
